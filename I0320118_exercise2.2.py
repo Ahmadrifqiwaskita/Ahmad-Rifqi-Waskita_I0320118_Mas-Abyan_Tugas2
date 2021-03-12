7eb55957cf7c646818de2bd5d5a422e065e1175f
Ahmadrifqiwaskita <80144753+Ahmadrifqiwaskita@users.noreply.github.com>
2021-03-12 18:11:30 +0700

    Add files via upload

---
+++ I0320118_exercise2.2.py
@@ -0,0 +1,29 @@
+#proses memasukan data ke dalam variabel
+nama =  "Rifqi Waskita"
+#print(nama)
+
+#nilai dan tipe data dalam variabel dapat diubah
+umur = 18
+print(umur)
+type(umur)
+umur = "delapan belas"
+print(umur)
+type(umur)
+
+namaDepan = "Ahmad"
+namaBelakang = "Rifqi"
+nama = namaDepan + " " + namaBelakang
+umur = 18
+hobi = "Bermain"
+print("Biodata\n", nama, "\n", umur, "\n", hobi)
+
+#contoh variabel lainnya
+inivariabel = "Halo"
+ini_juga_variabel = "Hai"
+_inivariabeljuga = "Hi"
+inivariabel222 = "Bye"
+
+panjang = 10
+lebar = 5
+luas = panjang * lebar
+print(luas)
