fe5a98e71c7567fe2b4e85d0d4e1fde5ddaafcde
Ahmadrifqiwaskita <80144753+Ahmadrifqiwaskita@users.noreply.github.com>
2021-03-12 22:22:22 +0700

    Add files via upload

---
+++ I0320118_soal1_tugas2.py
@@ -0,0 +1,64 @@
+#menampilkan informasi program
+print("Luas persegi panjang ")
+
+#input nilai panjang
+P = float(input("Masukkan nilai panjang: "))
+
+#input nilai lebar
+L = float(input("Masukkan nilai lebar: "))
+
+#menghitung luas persegi panjang
+luas_persegi_panjang = P * L
+
+#menampilkan hasil perhitungan ke layar
+print("luas persegi panjang:", luas_persegi_panjang)
+
+#menampilkan informasi program
+print("Luas lingkaran")
+
+#input nilai jari-jari
+r = float(input("Masukkan nilai jari-jari: "))
+
+#menghitung luas lingkaran
+luas_lingkaran = 3.14 * (r ** 2)
+
+#menampilkan hasil perhitungan kelayar
+print("luas lingkaran:", luas_lingkaran)
+
+#menampilkan informasi program
+print("Luas Kubus")
+
+#input nilai Sisi
+S = float(input("Masukkan nilai sisi: "))
+
+#menghitung luas kubus
+luas_kubus = S * S
+
+#menampilkan hasil perhitungan kelayar
+print("luas Kubus:", luas_kubus)
+
+#menampilkan informasi program
+print("konversi suhu (Celcius ke Fahrenheit)")
+
+#input suhu dalam Celcius
+C = float(input("Masukkan suhu (Celcius): "))
+
+#melakukan konversi suhu ke fahrenheit
+F = 9 * C / 5 +32
+
+#menampilkan hasil konversi ke layar
+print("Celcius: ", C)
+print("Fahrenheit:", F)
+
+#menampilkan informasi program
+print("konversi suhu (Reamur ke Kelvin)")
+
+#input suhu dalam Reamur
+R = float(input("Masukkan suhu(Reamur): "))
+
+#melakukan konversi suhu ke Kelvin
+K = 5 * R / 9 +273
+
+#menampilkan hasil konversi ke layar
+print("Reamur: ", R)
+print("Kelvin:",K)
---
+++ I0320118_soal2_tugas2.py
@@ -0,0 +1,28 @@
+#menampilkan informasi program
+
+print("PERKENALAN DIRI")
+
+nama = "Ahmad Rifqi Waskita"
+umur = 18
+tinggiBadan = 176
+beratBadan = float(80.5)
+alamatRumah = "Perumahan Puri Gading C3/12, Kota Bekasi"
+ukuranSepatu = float(44.5)
+hobi = "menonton film"
+agama = "Islam"
+jurusan = "Teknik Industri"
+Universitas = "Universitas Sebelas Maret"
+
+#menampilkan variabel ke terminal
+
+print("perkenalkan nama saya",nama)
+print("saat ini saya berusia",umur)
+print("tinggi badan saya yaitu",tinggiBadan)
+print("dan berat badan saya saat ini yaitu",beratBadan)
+print("saya bertempat tinggal di",alamatRumah)
+print("ukuran sepatu saya sekarang yaitu",ukuranSepatu)
+print("Hobi yang saya gemari saat ini adalah",hobi)
+print("saya memeluk agama", agama)
+print("saat ini saya berkuliah di jurusan",jurusan)
+print("di",Universitas)
+
